1047bc2b21a287aa5876d61bfa71d3e97993df30
DEBUrocks <originaldebu@gmail.com>
2021-02-07 11:31:37 +0530

    19:42 added components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { ChatEngine } from 'react-chat-engine';
-
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
 const App = () => {
@@ -9,6 +9,7 @@ const App = () => {
             projectID="9574fe28-d4aa-4020-8f76-51a311b3d8ec"
             userName="DebmeetAdmin1"
             userSecret="admin"
+            renderChatFeed={(chatAppProps)=> <ChatFeed { ... chatAppProps}/>}
         />
     );
 }
